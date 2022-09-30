abb191713ffe7de5fd5ff188a1e017c34440ea24
Dmytro Mykhailov <92753556+imavin08@users.noreply.github.com>
2022-09-30 21:35:10 +0300

    hw06 email is done

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -7,6 +7,8 @@ const {
   loginController,
   subscriptionController,
   changeAvatarController,
+  verificationController,
+  repeatVerifyController,
 } = require("../controllers/authController");
 
 const {
@@ -16,11 +18,18 @@ const {
   currentUserValidation,
   subscriptionValidation,
   tokenValidation,
+  repeatVerifyValidation,
 } = require("../midlewares/authMidleware");
 
 const { uploadMidlewares } = require("../midlewares/uploadMidlewares");
 
 router.post("/signup", signUpValidation, asyncWrapper(registrationController));
+router.get("/verify/:verificationToken", asyncWrapper(verificationController));
+router.post(
+  "/verify",
+  repeatVerifyValidation,
+  asyncWrapper(repeatVerifyController)
+);
 router.post("/login", loginValidation, asyncWrapper(loginController));
 router.get("/logout", logoutValidation);
 router.get("/current", currentUserValidation);
